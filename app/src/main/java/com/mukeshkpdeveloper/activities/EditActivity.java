5e30111e1a463856613dd65504b4a4ef75c45ab8
Mukesh Kumar <kumarmukeshpatel57@gmail.com>
2022-10-01 19:14:55 +0530

    Working

---
+++ app/src/main/java/com/mukeshkpdeveloper/activities/EditActivity.java
@@ -0,0 +1,116 @@
+package com.mukeshkpdeveloper.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mukeshkpdeveloper.constants.Constants;
+import com.mukeshkpdeveloper.database.AppDatabase;
+import com.mukeshkpdeveloper.database.AppExecutors;
+import com.mukeshkpdeveloper.model.Person;
+import com.mukeshkpdeveloper.offlineappdemo.R;
+
+
+public class EditActivity extends AppCompatActivity {
+    EditText name, email, pincode, city, phoneNumber;
+    Button button;
+    int mPersonId;
+    Intent intent;
+    private AppDatabase mDb;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        initViews();
+        mDb = AppDatabase.getInstance(getApplicationContext());
+        intent = getIntent();
+        if (intent != null && intent.hasExtra(Constants.UPDATE_Person_Id)) {
+            button.setText("Update");
+
+            mPersonId = intent.getIntExtra(Constants.UPDATE_Person_Id, -1);
+
+            AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    Person person = mDb.personDao().loadPersonById(mPersonId);
+                    populateUI(person);
+                }
+            });
+
+
+        }
+
+    }
+
+    private void populateUI(Person person) {
+
+        if (person == null) {
+            return;
+        }
+
+        name.setText(person.getName());
+        email.setText(person.getEmail());
+        phoneNumber.setText(person.getNumber());
+        pincode.setText(person.getPincode());
+        city.setText(person.getCity());
+    }
+
+    private void initViews() {
+        name = findViewById(R.id.edit_name);
+        email = findViewById(R.id.edit_email);
+        pincode = findViewById(R.id.edit_pincode);
+        city = findViewById(R.id.edit_city);
+        phoneNumber = findViewById(R.id.edit_number);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSaveButtonClicked();
+            }
+        });
+    }
+
+    public void onSaveButtonClicked() {
+        final Person person = new Person(
+                name.getText().toString(),
+                email.getText().toString(),
+                phoneNumber.getText().toString(),
+                pincode.getText().toString(),
+                city.getText().toString());
+
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                if (!intent.hasExtra(Constants.UPDATE_Person_Id)) {
+                    mDb.personDao().insertPerson(person);
+                } else {
+                    person.setId(mPersonId);
+                    mDb.personDao().updatePerson(person);
+                }
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
